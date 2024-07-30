4385ecc3b1c90c76bb31d495b653e09f90c280d1
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-30 16:13:02 +0300

    add placeholder data for testing ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,24 @@ import ContactList from './ContactList'
 import './App.css'
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([
+    {firstName: "Susan", lastName: "Pearl", email: "email", id: 1}
+  ])
 
-  useEffect(() => {
-    fetchContacts()
-  }, [])
+  // useEffect(() => {
+  //   fetchContacts()
+  // }, [])
 
-  const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
-  }
+  // const fetchContacts = async () => {
+  //   const response = await fetch("http://127.0.0.1:5000/contacts")
+  //   const data = await response.json()
+  //   setContacts(data.contacts)
+  //   console.log(data.contacts)
+  // }
 
-  return <ContactList  contacts={contacts}/>
+  return (
+    <ContactList  contacts={contacts}/>
+  )
 }
 
 export default App
