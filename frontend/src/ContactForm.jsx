b21107c833e2fe7b4cd84864afde286ab1c56520
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-30 16:45:53 +0300

    fix type error failed fetch line 16

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -13,7 +13,7 @@ const ContactForm = ({}) => {
       lastName,
       email,
     }
-    const url = 'htt://127.0.0.1:5000/create_contact'
+    const url = 'http://127.0.0.1:5000/create_contact'
     const options = {
       method: 'POST',
       headers: {
