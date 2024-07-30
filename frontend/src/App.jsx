7be1dc436facb5c9be6dca1fa1166daa7ac6d6ef
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-30 13:42:59 +0300

    fix typ error - failed fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   }, [])
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:500/contacts")
+    const response = await fetch("http://127.0.0.1:5000/contacts")
     const data = await response.json()
     setContacts(data.contacts)
     console.log(data.contacts)
